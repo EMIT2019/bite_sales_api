39c9788a80cf121ca0e9e18a9a4e164409322f24
EMIT2019 <emilianoandreurbinazavala@gmail.com>
2024-08-20 23:17:45 -0600

    Initial Commit

---
+++ src/main/java/com/emit/bite_sales/BiteSalesApplication.java
@@ -0,0 +1,13 @@
+package com.emit.bite_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emit/bite_sales/BiteSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emit.bite_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
